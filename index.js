702d9b04c37a0b9866363c8c4f74980ebd0907ab
assignment <prakitz2908@gmail.com>
2023-06-05 19:55:43 +0530

    added

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 // console.dir(app);
 // app.use((req,res)=>{
@@ -9,24 +9,28 @@ const app = express();
 // cats=> 'meow
 // dogs=>woof
 //  '/' homepage
-app.get('/r/:subreddit',(req,res)=>{ //we match matter
-    const {subreddit} = req.params;
-    res.send(`<h1>Browsing the ${subreddit} subreddit`);
-
-})
-app.get('/cats',(req,res)=>{
-    res.send('<h1>MEOW!!!</h1>')
-})
-app.get('/dogs',(req,res)=>{
-    res.send('<h1>WOFF!!</h1>')
-})
-app.get('/',(req,res)=>{
-    res.send('<h1>WELCOME TO BILLI AND KUTTA!!!</h1>')
-})
-
-app.listen(3000,()=>{
-    console.log("listening on 3000");
-})
-app.get('*',(req,res)=>{ //Generic response incase nothing matches
-    res.send('THIS REQUEST DOES NOT EXIST');
-})
+app.get("/r/:subreddit", (req, res) => {
+  //we match matter
+  const { subreddit } = req.params;
+  res.send(`<h1>Browsing the ${subreddit} subreddit`);
+});
+app.get("/cats", (req, res) => {
+  res.send("<h1>MEOW!!!</h1>");
+});
+app.get("/dogs", (req, res) => {
+  res.send("<h1>WOFF!!</h1>");
+});
+app.get("/", (req, res) => {
+  res.send("<h1>WELCOME TO BILLI AND KUTTA!!!</h1>");
+});
+app.get("/search", (req, res) => {
+  const {q}=req.query;
+  res.send(`<h1>Search Query : ${q}`);
+});
+app.listen(3000, () => {
+  console.log("listening on 3000");
+});
+app.get("*", (req, res) => {
+  //Generic response incase nothing matches
+  res.send("THIS REQUEST DOES NOT EXIST");
+});
